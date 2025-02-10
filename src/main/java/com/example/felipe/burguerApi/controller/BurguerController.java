fefff0222261a70958d342b3ed95d28ac193e68a
Felipe Alves <115760676+felipenewplayer@users.noreply.github.com>
2025-02-10 15:55:37 -0300

    api0.2

--- src/main/java/com/example/felipe/burguerApi/controller/BurguerController.java
+++ src/main/java/com/example/felipe/burguerApi/controller/BurguerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/burguer")
+@RequestMapping()
 @CrossOrigin("http://localhost:5173/")
 public class BurguerController {
 
