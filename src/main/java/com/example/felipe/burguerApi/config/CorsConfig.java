d189a61f889e5e7734a7d32a162f74816a9ecadb
Felipe Alves <115760676+felipenewplayer@users.noreply.github.com>
2025-02-16 18:27:54 -0300

    arrumando_api3

--- src/main/java/com/example/felipe/burguerApi/config/CorsConfig.java
+++ src/main/java/com/example/felipe/burguerApi/config/CorsConfig.java
@@ -14,11 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-<<<<<<< HEAD
         config.setAllowedOrigins(List.of("https://castilhos-lta49ocml-felipenewplayers-projects.vercel.app/","http://localhost:5173"));
-=======
-    config.setAllowedOrigins(List.of("https://castilhos-lta49ocml-felipenewplayers-projects.vercel.app", "https://castilhos-lta49ocml-felipenewplayers-projects.vercel.app/signUp"));
->>>>>>> ab87624bf59efd5cedce7d06639b904071b97efc
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
