779ead146d7c7a9139e2a94d60cb96bf7b7a0e08
felipenewplayer <felipeapereira2014@gmail.com>
2025-05-09 10:47:53 -0300

    arrumado

--- src/main/java/com/example/felipe/burguerApi/service/BurguerService.java
+++ src/main/java/com/example/felipe/burguerApi/service/BurguerService.java
@@ -3,7 +3,6 @@ package com.example.felipe.burguerApi.service;
 import com.example.felipe.burguerApi.model.Burguer;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Service
