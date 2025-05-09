c46d78279a9abe8811c3554e9096a237c9fd5f46
Felipe Alves <115760676+felipenewplayer@users.noreply.github.com>
2025-05-09 14:59:30 -0300

    Update CorsConfig.java

--- src/main/java/com/example/felipe/burguerApi/config/CorsConfig.java
+++ src/main/java/com/example/felipe/burguerApi/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://castilhos-ej6vycbei-felipenewplayers-projects.vercel.app/","http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://castilhosbar.netlify.app/","http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
